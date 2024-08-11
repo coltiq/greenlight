0200ece1a01a043e729d43aaa00fe386f1324204
coltiq <dev@coltiq.com>
2024-08-11 14:52:51 -0500

    Add: Error Handling / Middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,9 +9,12 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler) // Show application information
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)     // Create a new movie
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)    // Show the details of a specific movie
 
-	return router
+	return app.recoverPanic(router)
 }
