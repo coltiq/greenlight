926bf5ea43c7c6de5cef217e3d8fe7f61277c3fe
coltiq <dev@coltiq.com>
2024-08-11 14:52:32 -0500

    Add: Movie Data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"log/slog"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		slog.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
